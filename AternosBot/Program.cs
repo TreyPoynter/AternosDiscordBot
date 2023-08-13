a5477787dcc881d63091264b5b5fdd84fc7b828a
TreyPoynter <treypoynter@gmail.com>
2023-08-13 08:36:24 -0700

    push bot code

---
+++ AternosBot/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AternosBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bot bot = new Bot();
+            bot.RunAsync().GetAwaiter().GetResult();
+
+		    Console.ReadLine();
+        }
+    }
+}
---
+++ AternosBot/bin/Debug/net6.0/CallAPI.py
@@ -0,0 +1,33 @@
+﻿# Import
+import sys
+from python_aternos import Client
+from python_aternos import ServerStartError
+
+# Log in
+aternos = Client.from_credentials('The_Imposter2', 'Nowayamongus!') # User, Pass
+
+servs = aternos.list_servers()
+
+myserv = servs[0]
+
+currServer = None
+for serv in servs:
+    if serv.address == 'Metrovania_2.aternos.me': # IP
+        currServer = serv
+
+serverIsStarted = False;
+
+if currServer is not None:
+   try:
+        currServer.start()
+        serverIsStarted = True;
+   except ServerStartError as err:
+        print(err.code)
+        print(err.message)
+        serverIsStarted = False;
+else:
+    print('Server not found')
+
+if serverIsStarted:
+    print(serverIsStarted, end="")
+
